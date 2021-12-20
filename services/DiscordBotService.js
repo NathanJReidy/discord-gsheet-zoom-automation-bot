fd81cd6bb41d4a5681db61dfcbae4d739f87e008
Nathan Reidy <nathanjreidy@gmail.com>
2021-12-20 21:07:12 +1100

    feat: added DiscordBotService

---
+++ services/DiscordBotService.js
@@ -0,0 +1,29 @@
+import { Client, Intents } from "discord.js";
+import dotenv from "dotenv";
+import axios from "axios";
+dotenv.config();
+
+export class DiscordService {
+  async startAdminBot() {
+    const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS] });
+    const id = process.env.TEST_GUILD_ID;
+    client.login(process.env.DISCORD_BOT_TOKEN);
+
+    client.on("ready", async () => {
+      // if channel.id !== #administrative then disable commands and return a
+      // message saying this bot is only available to admins
+      // if channel.id === #administrative then allow all commands
+
+      const guild = client.guilds.cache.find((guild) => guild.id === id);
+
+      if (!guild) {
+        throw new Error(`Can't find any guild with the ID "${id}"`);
+      }
+
+      const allDiscordMembers = await guild.members.fetch();
+      const allDiscordMembersUsernames = allDiscordMembers.map(
+        (member) => member.user.username
+      );
+    });
+  }
+}
