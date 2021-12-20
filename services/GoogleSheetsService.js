eea7bd4277f2a15e9ab1941eef0419a250f43252
Nathan Reidy <nathanjreidy@gmail.com>
2021-12-20 17:24:11 +1100

    feat: added GoogleSheetsService

---
+++ services/GoogleSheetsService.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export class GoogleSheetsService {
+  /**
+   * This returns all the Discord Usernames from
+   * the Google Sheet, which will be everyone who has
+   * booked an onboarding call.
+   * */
+  async getAllGoogleSheetsDiscordUsernames() {
+    // do stuff
+  }
+}
