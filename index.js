132e47b55c8286099428afb35fccf5457f04c8fe
Nathan Reidy <nathanjreidy@gmail.com>
2021-12-20 21:06:19 +1100

    chore: updated packages

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import { DiscordService } from "./services/DiscordService.js";
 import { UtilitiesService } from "./services/UtilitiesService.js";
+import { GoogleSheetsService } from "./services/GoogleSheetsService.js";
 
 const discordService = new DiscordService();
-console.log(await discordService.getDiscordUsernames());
+const allDiscordUsernames = await discordService.getAllDiscordUsernames();
 
-const utilitiesService = new UtilitiesService();
-// console.log(await utilitiesService.findDiscordUsernamesWithoutBookedCall());
+// const utilitiesService = new UtilitiesService();
+
+const googleSheetsService = new GoogleSheetsService();
+const allSpreadSheetDiscordUsernames =
+  await googleSheetsService.getAllSpreadSheetDiscordUsernames();
+
+console.log(
+  await discordService.findDiscordUsernamesWithoutBookedCall(
+    allDiscordUsernames,
+    allSpreadSheetDiscordUsernames
+  )
+);
