3745d5d290568b1459c4a76430c69cd7a1a30c3e
Nathan Reidy <nathanjreidy@gmail.com>
2022-01-14 16:10:15 +1100

    feat: updated config details in .env and config folder

---
+++ dist/config/service_account_credentials.d.ts
@@ -0,0 +1,12 @@
+export declare const GOOGLE_APPLICATION_CREDENTIALS: {
+    type: string | undefined;
+    project_id: string | undefined;
+    private_key_id: string | undefined;
+    private_key: string | undefined;
+    client_email: string | undefined;
+    client_id: string | undefined;
+    auth_uri: string | undefined;
+    token_uri: string | undefined;
+    auth_provider_x509_cert_url: string | undefined;
+    client_x509_cert_url: string | undefined;
+};
---
+++ dist/config/service_account_credentials.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GOOGLE_APPLICATION_CREDENTIALS = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.GOOGLE_APPLICATION_CREDENTIALS = {
+    type: process.env.SERVICE_TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+};
+//# sourceMappingURL=service_account_credentials.js.map
